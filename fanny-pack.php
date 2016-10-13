e3c8ab590a7c9aa6377d9e6c5f7a563b1f97dd66
JJ Loomis <jjloomis@users.noreply.github.com>
2016-10-13 13:39:03 -0500

    Add files via upload

---
+++ fanny-pack.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Fanny Pack
+Plugin URI:  https://github.com/jjloomis/fanny-pack
+Description: Run Jetpack in Development Mode. No WordPress.com account required.
+Version:     0.0
+Author:      JJ Loomis
+Author URI:  https://github.com/jjloomis
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Filter to run Jetpack in Development Mode.
+add_filter( 'jetpack_development_mode', '__return_true' );
